5a7c82def2569a6823f7426ce6432a0cecfa6a48
Amin Ahmed Amin <aminahmed56344@gmail.com>
2025-07-21 03:51:28 +0300

    add rdais

--- VuexyBase.Infrastructure/Infrastructure/Services/RedisCache/RedisCacheService.cs
+++ VuexyBase.Infrastructure/Infrastructure/Services/RedisCache/RedisCacheService.cs
@@ -8,23 +8,23 @@ using VuexyBase.Application.Application.Contracts.Infrastructure.RedisCache;
 
 namespace VuexyBase.Infrastructure.Infrastructure.Services.RedisCache
 {
-    public class RedisCacheService : IRedisCacheService
-    {
-        private readonly IDatabase _database;
+    //public class RedisCacheService : IRedisCacheService
+    //{
+    //    private readonly IDatabase _database;
 
-        public RedisCacheService(IConnectionMultiplexer redis)
-        {
-            _database = redis.GetDatabase();
-        }
+    //    public RedisCacheService(IConnectionMultiplexer redis)
+    //    {
+    //        _database = redis.GetDatabase();
+    //    }
 
-        public async Task<string?> GetValueAsync(string key)
-        {
-            return await _database.StringGetAsync(key);
-        }
+    //    public async Task<string?> GetValueAsync(string key)
+    //    {
+    //        return await _database.StringGetAsync(key);
+    //    }
 
-        public async Task SetValueAsync(string key, string value, TimeSpan? expiry = null)
-        {
-            await _database.StringSetAsync(key, value, expiry ?? TimeSpan.FromHours(1));
-        }
-    }
+    //    public async Task SetValueAsync(string key, string value, TimeSpan? expiry = null)
+    //    {
+    //        await _database.StringSetAsync(key, value, expiry ?? TimeSpan.FromHours(1));
+    //    }
+    //}
 }
